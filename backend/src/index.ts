2d0dd049f36a553e1788f67015d215cac9d2c6d3
Dawei Si <dawei.si@schrodinger.com>
2024-09-05 17:23:38 -0400

    Started working on discord bot.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,23 @@
-throw new Error('Not implemented')
+import { createBot, Intents } from '@discordeno/bot'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const bot = createBot({
+  token: process.env.DISCORD_BOT_TOKEN!,
+  intents: Intents.Guilds | Intents.GuildMessages, // Or other intents that you might needs.
+  events: {
+    ready: (data) => {
+      console.log(`The shard ${data.shardId} is ready!`)
+    },
+  },
+})
+
+// You can add events after the createBot call if you prefer
+
+bot.events.messageCreate = (message) => {
+  // Do stuff with the message object ...
+  console.log(message.content)
+}
+
+await bot.start()
